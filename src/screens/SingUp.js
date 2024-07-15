93bc32b2e4a25cae616165f8e87d58c1c84c0c6e
sochiiii <20220018@ricaldone.edu.sv>
2024-07-15 10:54:22 -0600

    Diseño de registro

---
+++ src/screens/SingUp.js
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
+import {
+  TextInput,
+  Button,
+  PaperProvider,
+  Card,
+} from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import fetchData from '../../api/components';
+
+//Constante para manejar el alto de la pantalla
+const windowHeight = Dimensions.get("window").height;
+
+const RegisterScreen = () => {
+  //Url de la api
+  const USER_API = "servicios/publica/cliente.php";
+  //Constantes para el manejo de datos
+  const [correo, setCorreo] = useState("");
+  const [clave, setClave] = useState("");
+
+  //Constante de navegación entre pantallas
+  const navigation = useNavigation();
+
+  //Metodo para manejar el registro de usuarios
+  const handleRegister = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("correoRegistro", correo);
+      formData.append("claveRegistro", clave);
+
+      //Petición a la api para insertar un usuario
+      const response = await fetchData(USER_API, "signUpMovli", formData);
+
+      if (response.status) {
+
+      } else {
+
+      }
+    } catch (error) {
+
+    }
+  };
+
+
+  return (
+    <PaperProvider>
+      <ImageBackground
+        source={require("../../assets/fondo-change.png")}
+        style={styles.backgroundImage}
+      ></ImageBackground>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Card style={styles.profileCard}>
+            <Card.Content>
+              <View style={styles.inputContainer}>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Correo electrónico:</Text>
+                  <View style={styles.rowContent}>
+                    <AntDesign name="mail" size={24} />
+                    <TextInput
+                      style={styles.infoText}
+                      value={correo}
+                      onChangeText={setCorreo}
+                      keyboardType="email-address"
+                    />
+                  </View>
+                </View>
+              </View>
+              <View style={styles.inputContainer}>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Clave del cliente:</Text>
+                  <View style={styles.rowContent}>
+                    <Entypo name="lock" size={24} />
+                    <TextInput
+                      style={styles.infoText}
+                      value={clave}
+                      onChangeText={setClave}
+                      secureTextEntry={true}
+                    />
+                  </View>
+                </View>
+              </View>
+              <Button
+                style={styles.button}
+                mode="contained"
+                onPress={handleRegister}
+              >
+                Registrarse
+              </Button>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("LoginScreen")}
+              >
+                <Text style={styles.loginText}>
+                  ¿Ya tienes cuenta? Inicia sesión
+                </Text>
+              </TouchableOpacity>
+            </Card.Content>
+          </Card>
+        </View>
+      </ScrollView>
+    </PaperProvider>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    marginBottom: windowHeight * 0.15,
+    paddingTop: 50,
+  },
+  profileCard: {
+    width: "100%",
+    marginTop: 10,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#B7DABE",
+    paddingTop: 20,
+    paddingBottom: 40,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 14,
+    color: "gray",
+    marginBottom: 5,
+  },
+  infoRow: {
+    padding: 12,
+    margin: 2,
+    borderRadius: 10,
+    backgroundColor: "white",
+    width: "100%",
+    elevation: 2,
+  },
+  rowContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  infoText: {
+    marginLeft: 10,
+    fontSize: 16,
+    backgroundColor: "transparent",
+    height: 40,
+    borderWidth: 0,
+    flex: 1,
+  },
+  pickerText: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    color: "black",
+    flex: 1,
+  },
+  fila: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button: {
+    width: "100%",
+    paddingVertical: 10,
+    marginTop: 10,
+    backgroundColor: "#38A34C",
+  },
+  loginText: {
+    marginTop: 20,
+    color: "black",
+  },
+  avatarContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  avatarImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  backgroundImage: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+});

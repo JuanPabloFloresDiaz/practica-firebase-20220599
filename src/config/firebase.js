9aab2fa4ec1395c0bf5b899c0239e3cba3a87d92
sochiiii <20220018@ricaldone.edu.sv>
2024-07-16 09:48:55 -0600

    Cambios de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,64 +2,50 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, TEST } from '@env';
-
-console.log(TEST);
+ 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID  
 };
-
+ 
+console.log("Valor de configuracion", firebaseConfig);
+ 
 // Initialize Firebase
-let app;
-if (getApps().length === 0) {
-  app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+if (app) {
   console.log('Firebase initialized successfully');
 } else {
-  app = getApp();
-  console.log('Firebase already initialized');
+  console.log('Firebase initialization failed');
 }
-
+ 
 const database = getFirestore(app);
 if (database) {
   console.log('Firestore initialized correctly');
 } else {
   console.log('Firestore initialization failed');
 }
-
+ 
 const storage = getStorage(app);
+ 
 if (storage) {
-  console.log('storage initialized correctly');
+  console.log('Storage initialized correctly');
 } else {
-  console.log('storage initialization failed');
+  console.log('Storage initialization failed');
 }
-
-let auth;
-try {
-  auth = getAuth(app);
-  console.log('auth already initialized');
-} catch (e) {
-  if (e.code === 'auth/already-initialized') {
-    auth = getAuth(app);
-    console.log('using existing auth instance');
-  } else {
-    auth = initializeAuth(app, {
-      persistence: getReactNativePersistence(AsyncStorage)
-    });
-    console.log('auth initialized with persistence');
-  }
-}
-
+ 
+const auth = getAuth(app); // Inicializar Auth
+ 
 if (auth) {
-  console.log('auth initialized correctly');
+  console.log('Auth initialized correctly');
 } else {
-  console.log('auth initialization failed');
+  console.log('Auth initialization failed');
 }
-
+ 
 export { database, storage, auth };
